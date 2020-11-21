68dbb398f3fe9116402736beb088d77ea30f0614
angelo milien <angelomilienjoseph@gmail.com>
2020-11-21 05:29:19 -0500

    done

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,5 +1,4 @@
 
-require "pry"
 
 class MusicLibraryController
     
@@ -12,36 +11,44 @@ class MusicLibraryController
 
     def call
         input = ""
-        while input != 'exit'
-        puts "Welcome to your music library!"
-        puts "To list all of your songs, enter 'list songs'."
-        puts "To list all of the artists in your library, enter 'list artists'."
-        puts "To list all of the genres in your library, enter 'list genres'."
-        puts "To list all of the songs by a particular artist, enter 'list artist'."
-        puts "To list all of the songs of a particular genre, enter 'list genre'."
-        puts "To play a song, enter 'play song'."
-        puts "To quit, type 'exit'."
-        puts "What would you like to do?"
+        while input
+            puts ""
+             puts "Welcome to your music library!"
+             puts "To list all of your songs, enter 'list songs'."
+             puts "To list all of the artists in your library, enter 'list artists'."
+             puts "To list all of the genres in your library, enter 'list genres'."
+             puts "To list all of the songs by a particular artist, enter 'list artist'."
+             puts "To list all of the songs of a particular genre, enter 'list genre'."
+             puts "To play a song, enter 'play song'."
+             puts "To quit, type 'exit'."
+             puts "What would you like to do?"
         
-        input = gets.chomp
+            input = gets.chomp
     
-        case input
-            when 'list songs'
-              self.list_songs
-            when 'list artists'
-              self.list_artists
-            when 'list genres'
-              self.list_genres
-            when 'list artists'
-              self.list_artists
-            when 'list artist'
-              self.list_songs_by_artist
-            when 'list genre'
-              self.list_songs_by_genre
-            when 'play song'
-              self.play_song
-            else
-              "Type in a valid request please"
+            case input
+                when 'list songs'
+                  self.list_songs
+                  puts ""
+                when 'list artists'
+                  self.list_artists
+                  puts ""
+                when 'list genres'
+                  self.list_genres
+                  puts ""
+                when 'list artist'
+                  self.list_songs_by_artist
+                  puts ""
+                when 'list genre'
+                  self.list_songs_by_genre
+                  puts ""
+                when 'play song'
+                  self.play_song
+                  puts ""
+                when "exit"
+                    break
+                else
+                  puts "Type in a valid request please"
+                  puts ""
             end
         end
     end
